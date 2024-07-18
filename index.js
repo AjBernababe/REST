4df32aa3b079eea0e0dc8d16bf2d6cb6c9114dae
AjBernababe <138166844+AjBernababe@users.noreply.github.com>
2024-07-18 23:32:47 +0800

    updated html

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import express from 'express';
 const port = 8080;
 const app = express();
 
-app.get('/tacos', (req, rest) => {
+app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    res.send("POST /tacos response")
 })
 
 app.listen(port, () => {

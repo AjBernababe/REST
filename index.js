311f720c02432be0751b14361405694f61ba159e
AjBernababe <138166844+AjBernababe@users.noreply.github.com>
2024-07-19 00:00:41 +0800

    added html as public

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const port = 8080;
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
